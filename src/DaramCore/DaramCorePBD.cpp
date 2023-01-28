518d1e447ea4c25e4a022a7b2af68f2d08d01299
Daramgieer <cjw950321@gmail.com>
2023-01-28 21:09:45 +0900

    add metal api start

---
+++ src/DaramCore/DaramCorePBD.cpp
@@ -0,0 +1,9 @@
+#include <cassert>
+
+#define NS_PRIVATE_IMPLEMENTATION
+#define MTL_PRIVATE_IMPLEMENTATION
+#define MTK_PRIVATE_IMPLEMENTATION
+#define CA_PRIVATE_IMPLEMENTATION
+#include <Metal/Metal.hpp>
+#include <AppKit/AppKit.hpp>
+#include <MetalKit/MetalKit.hpp>
